132f2c4a87513b6a5ad4762f17f6afd441024846
Boutallaka Soufiane <s.boutallaka@gmail.com>
2016-12-07 12:19:10 +0100

    phaser

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,22 @@ const gulp    = require ('gulp');
 const uglify  = require ('gulp-uglify');
 const concat  = require ('gulp-concat');
 const babel   = require ('gulp-babel');
+const sass = require('gulp-sass');
 
 const plumber = require ('gulp-plumber');
 const reload  = require ('gulp-livereload');
 
+
 const vendors = require ('./vendors.js');
 
+gulp.task('sass', function () {
+  return gulp.src('src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src'));
+});
+
 gulp.task ('html', function ()  {
-  gulp.src (['src/*.html' ,'src/*.json' ])
+  gulp.src (['src/*.html' ,'src/*.json','img/*.png' ])
       .pipe (gulp.dest('build'));
 });
 
@@ -28,7 +36,9 @@ gulp.task ('custom-js', function () {
       .pipe (uglify())
       .pipe (concat('custom.js'))
       .pipe (gulp.dest('build'));
-});
+    });
+
+
 
 gulp.task ('vendor', function () {
   gulp.src (vendors.js)
@@ -37,7 +47,7 @@ gulp.task ('vendor', function () {
       .pipe (gulp.dest('build'));
 });
 
-gulp.task ('build', ['custom-css', 'custom-js', 'html']);
+gulp.task ('build', ['custom-css', 'custom-js', 'html','sass']);
 gulp.task ('build-all', ['build', 'vendor']);
 
 gulp.task ('watch', ['build'], function () {
